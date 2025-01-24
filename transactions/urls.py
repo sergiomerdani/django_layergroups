40fa9a9fb6484f4935bad4a6a37a01a5db3db294
Sergio Merdani <sergiomerdani@gmail.com>
2025-01-24 16:46:33 +0100

    Delete feature WFS Transaction DONE

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DrawFeatureAPIView
+from .views import DrawFeatureAPIView,DeleteFeatureAPIView
 
 urlpatterns = [
     path("add-feature/", DrawFeatureAPIView.as_view(), name="add-feature"),
+    path("delete-feature/", DeleteFeatureAPIView.as_view(), name="delete-feature"),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -115,3 +115,55 @@ class DrawFeatureAPIView(APIView):
             """
         else:
             raise ValueError("Unsupported geometry type")
+
+
+class DeleteFeatureAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        # Extract parameters from the request payload
+        layer_name = request.data.get("layerName")
+        workspace = request.data.get("workspace")
+        feature_id = request.data.get("featureID")  # The ID of the feature to delete
+        host = request.data.get("host", "localhost")  # Default to localhost if not provided
+
+        if not (layer_name and workspace and feature_id):
+            return Response(
+                {"error": "Missing required parameters: layerName, workspace, or featureID"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        # Generate the WFS Delete Transaction XML
+        try:
+            wfs_transaction = self.generate_wfs_delete_transaction(layer_name, feature_id)
+        except ValueError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Send the WFS Transaction request to GeoServer
+        geoserver_url = f"http://{host}:8080/geoserver/{workspace}/ows"
+        headers = {"Content-Type": "text/xml"}
+
+        try:
+            response = requests.post(geoserver_url, data=wfs_transaction, headers=headers)
+            if response.status_code == 200:
+                return Response({"message": "Feature deleted successfully", "details": response.text}, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "GeoServer error", "details": response.text}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def generate_wfs_delete_transaction(self, layer_name, feature_id):
+        return f"""
+        <wfs:Transaction service="WFS" version="1.0.0"
+        xmlns:cdf="http://www.opengis.net/cite/data"
+        xmlns:ogc="http://www.opengis.net/ogc"
+        xmlns:wfs="http://www.opengis.net/wfs"
+        xmlns:topp="http://www.openplans.org/topp">
+            <wfs:Delete typeName="{layer_name}">
+                <ogc:Filter>
+                    <ogc:PropertyIsEqualTo>
+                        <ogc:PropertyName>id</ogc:PropertyName>
+                        <ogc:Literal>{feature_id}</ogc:Literal>
+                    </ogc:PropertyIsEqualTo>
+                </ogc:Filter>
+            </wfs:Delete>
+        </wfs:Transaction>
+        """
